2ad79b6b94067d2d0600c411be6a243f7d765a7c
avalchev94 <asen.valchev1@gmail.com>
2018-05-11 18:20:48 +0300

    Implemented UI part
    Finished the basic implementation of the UI

--- main.go
+++ main.go
@@ -2,35 +2,59 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/avalchev94/boolean-evaluator/evaluator"
 	"github.com/avalchev94/hn-hiring/hackernews"
+	"github.com/gorilla/websocket"
 	"log"
+	"net/http"
+	"path/filepath"
 	"strings"
+	"text/template"
 )
 
-func main() {
-	postID := flag.Int64("post", 16735011, "ID referencing \"who is hiring\" post.")
-	flag.Parse()
+const (
+	templateFolder = "templates"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	path := filepath.Join(templateFolder, "hire.html")
+	templ := template.Must(template.ParseFiles(path))
+
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+
+	templ.Execute(w, data)
+}
+
+var postID = flag.Int64("post", 16735011, "ID referencing \"who is hiring\" post.")
+var upgrader = websocket.Upgrader{}
+
+func hireHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer c.Close()
 
 	p, err := hackernews.QueryPost(*postID)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println(p.Kids)
-	found := make(chan *hackernews.Post)
+
+	found := make(chan *hackernews.Post, 10)
 	go func() {
 		for p := range found {
-			fmt.Printf("Post found with ID %d and Title %s.\n", p.ID, p.Title)
+			c.WriteJSON(*p)
 		}
 	}()
 
-	eval, err := evaluator.New("Remote & (C|Go|Golang)")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 	searchFunc := func(title, text string) bool {
+		eval, err := evaluator.New("Remote & (Go|Golang)")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
 		for param, _ := range eval.Parameters {
 			eval.Parameters[param] = strings.Contains(text, param)
 		}
@@ -44,6 +68,16 @@ func main() {
 
 	p.SearchKids(searchFunc, found, 4)
 	close(found)
+}
 
-	fmt.Printf("Asenaki")
+func main() {
+
+	flag.Parse()
+
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/hire", hireHandler)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalln(err)
+	}
 }
